8e98344ee749fd2f843c9e9d9f46252001833aa4
pavel.klok <bcharly49@gmail.com>
2021-01-10 16:15:47 +0200

    add tests

--- promise/promise.js
+++ promise/promise.js
@@ -1,5 +1,48 @@
-class MyPromise {
+// https://youtu.be/SQMCtWnCxEA
 
+class MyPromise {
+    constructor(executor) {
+        if (typeof executor === 'function'){
+            this.queue = [];
+            this.error = ()=>{};
+            executor.call(null, this.onResolve.bind(this), this.onReject.bind(this));
+        }
+    }
+
+    onResolve(data){
+        this.queue.forEach(callback=>{
+            data = callback(data);
+        });
+    }
+    onReject(err){
+        this.error(err);
+    }
+
+    then(fn){
+        this.queue.push(fn);
+        return this;
+    }
+
+    catch(fn){
+        this.error = fn ;
+        return this;
+    }
+
+    finally(fn){}
 }
 
 export default MyPromise
+
+// const promise = new MyPromise((resolve)=>{
+//     setTimeout(()=>{
+//         console.log('hello 33')
+//         resolve(42);
+//     }, 1500)
+//
+// })
+//
+// promise.then((response)=>{
+//     setTimeout(()=>{
+//         console.log('response=', response)
+//     }, 1500)
+// })
--- promise/promise.test.js
+++ promise/promise.test.js
@@ -1,13 +1,41 @@
 import MyPromise from "./promise";
+import 'regenerator-runtime/runtime';
 const myPromise = MyPromise;
 
 describe('my Promise:', ()=>{
 
-    test('promise should exist and function', ()=>{
+    const testNum= 44;
+    let promise, executorFunc
 
+    beforeEach(()=>{
+        executorFunc = jest.fn((resolve)=>{
+            return setTimeout(()=>{
+                return resolve(testNum);
+            },1500);
+        });
+        promise = new myPromise(executorFunc);
+    })
+
+    test('promise should exist and function', ()=>{
         expect(myPromise).toBeDefined();
         expect(typeof myPromise).toBe('function');
-
     });
+
+    test('should have methods', ()=>{
+        expect(promise.then).toBeDefined();
+        expect(promise.catch).toBeDefined();
+        expect(promise.finally).toBeDefined();
+    });
+
+    test('should call executor function', ()=>{
+        expect(executorFunc).toBeCalled();
+    });
+
+    test('should get data and chain', async ()=>{
+        const result = await promise.then(res=> res *2 );
+
+        expect(result).toBe(testNum * 2);
+    });
+
 
 })
